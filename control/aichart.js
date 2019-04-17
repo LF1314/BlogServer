3ad999984ed669b0d99579593daa9b3168f04247
LF1314 <1279826137@qq.com>
2019-04-17 17:58:07 +0800

    更新页面

--- control/aichart.js
+++ control/aichart.js
@@ -17,7 +17,7 @@ router.post('/',auth, async(req,res)=>{
     let content = req.body.content || '你好！'
     let quers ={
             info: content,
-            key:'9447b5568c0a4199a77809bfebb5d322',
+            key:'eed20e45343a4b30a3a2172b2dc13ae6',
             userId:'431409'   
         }
         let reply = await axios.post(api,quers) 
